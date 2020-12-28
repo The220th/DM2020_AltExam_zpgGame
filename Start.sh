4a9f4f169703e2114cd14f9534dc7bd825ca0496
The220th <the220th@>
2020-12-28 16:11:13 +0300

    Added sh

---
+++ Start.sh
@@ -0,0 +1,2 @@
+javac -encoding utf-8 -Xlint:unchecked ./ZPG/GUItest.java
+java -Xms4096m -Xmx8192m ZPG.GUItest
---
+++ StartNoGUI.sh
@@ -0,0 +1,2 @@
+javac -encoding utf-8 -Xlint:unchecked ./ZPG/GameLogic/GameHundler.java
+java  -Xms4096m -Xmx8192m ZPG.GameLogic.GameHundler
