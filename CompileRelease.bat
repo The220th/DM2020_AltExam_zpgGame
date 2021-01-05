3754d70b5519b60fc9fe9e2218f6fa7ea814ba9e
The220th <the220th@>
2021-01-06 00:18:24 +0300

    Added CompileRelease.bat

---
+++ CompileRelease.bat
@@ -0,0 +1,3 @@
+jar cvf0m .\release.jar MANIFEST.MF .\ZPG\*.class .\ZPG\sMap\*.class .\ZPG\MapGenerator\*.class .\ZPG\GameLogic\*.class .\ZPG\GameLogic\Quests\*.class .\ZPG\GameLogic\Searchers\*.class
+Pause
+CMD /Q /K
